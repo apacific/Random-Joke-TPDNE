d9c268487d38c79ffceee2a795b81c50e6c012a7
Anthony Pacific <anthony.pacific@seo-usa.org>
2021-07-26 17:43:58 +0000

    test folder

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, Markup, redirect, render_template, url_for
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  
+    data = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit").json().items()
+    for key, value in data:
+  
+        if key == 'joke':
+            joke = value
+        elif key == 'setup':
+            joke = value
+        elif key == 'delivery':
+            joke = Markup(joke + 3 * '<br />' + value)
+
+    return render_template('home.html', subtitle='PLEASE ENJOY THIS JOKE', text='as told by a person that doesn\'t exist:', joke=joke)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
