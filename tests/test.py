dbe8a3dd4a75c19c29ced5a2f68aa120a90cf885
Anthony Pacific <anthony.pacific@seo-usa.org>
2021-07-26 19:14:39 +0000

    test file

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+import unittest
+from main import home
+
+class test(unittest.TestCase):
+    def test_home_page(self):
+        self.assertEqual(function1(1), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
