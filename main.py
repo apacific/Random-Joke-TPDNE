15059a4119289e269a1f1ee68daea62701bc8ba1
Anthony Pacific <anthony.pacific@seo-usa.org>
2021-07-26 18:35:40 +0000

    linting and code style

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def home():
 
     return render_template('home.html', subtitle='PLEASE ENJOY THIS JOKE', text='as told by a person that doesn\'t exist:', joke=joke)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 
     app.run(debug=True, host="0.0.0.0")
