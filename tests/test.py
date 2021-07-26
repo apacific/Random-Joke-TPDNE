95f1637374a58e572e3265cf63a98cf4b6131577
Anthony Pacific <anthony.pacific@seo-usa.org>
2021-07-26 21:45:39 +0000

    test test

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,20 @@
 import unittest
 import requests
+from unittest.mock import Mock
 from main import home
 
+requests = Mock()
+
 class test(unittest.TestCase):
-    data = requests.get("https://v2.jokeapi.dev/joke"
-                        "/Programming").json().items()
+    data = requests.get('https://v2.jokeapi.dev/joke/Programming')
     
     def test_home_page(self):
         self.assertEqual(1+1, 2)
+        print(dir(requests))  # This shows us available methods to use with an object.
+        print(requests.get.assert_called())
+        print(requests.get.assert_not_called())
+        print(requests.get.assert_called_with('https://pokeapi.co/api/v2/pokemon/pikachu'))
+        print(requests.method_calls)
 
 
 if __name__ == '__main__':
