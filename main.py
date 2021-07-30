cbfdd860a02cb797ce07330511ec9b67f6517b15
Anthony Pacific <anthony.pacific@seo-usa.org>
2021-07-30 19:15:16 +0000

    requests import

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, Markup, redirect, render_template, url_for, requests
+from flask import Flask, Markup, redirect, render_template, url_for
+import requests
 import json
 
 app = Flask(__name__)
